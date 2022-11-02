222ac797eef2c42fa63a44dfcf2b8330aeb9ff8a
정재원 <one@jeongjaewon-ui-MacBookAir.local>
2022-11-03 01:03:13 +0900

    solved: Algorithm_221103

---
+++ Algorithm/Section_6/1-올바른_괄호.js
@@ -0,0 +1,29 @@
+/**
+ * ABOUT
+ *
+ * DATE: 2022-11-03
+ * AUTHOR: jungjaedev
+ *
+ * COMMENT:
+ * 올바른 괄호
+ **/
+
+ function solution(str) {
+  let answer = "true";
+  let stack = [];
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] === '(') {
+      stack.push(str[i]);
+    } else {
+      if(stack.length === 0) return "no";
+      stack.pop();
+    }
+  }
+  if(stack.length > 0) return "no";
+  return answer;
+}
+
+
+const str = "(()(()))(()";
+
+console.log(solution(str));
